a217a3683dffae2b0ae42ed68bd76059e3c3c8b7
Thiago Machado <tmac.ocn@gmail.com>
2015-10-01 13:44:44 -0300

    figura original e figura crescente protas

--- Thiago-Diana/bubble-sort.py
+++ Thiago-Diana/bubble-sort.py
@@ -4,10 +4,18 @@ lista = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8] #
 print("lista original", lista)
 N = 20 # numero de termos 
 
+import matplotlib.pyplot as plt
+plt.figure ()
+plt.plot(range(0, N, 1), lista,'ok') 
+plt.title("Original")
+plt.xlabel("posicao")
+plt.ylabel("valores")
+plt.savefig("fig/bubble-inicio.png")
+
 # i e j sao os elementos posicionados
-# o range para a letra i é o intervalo entre a primeira posição (0), e a ultima (N-1),
-# em relação a j, o primeiro termo foi indicado como "for i", por isso começamos com o comando i+j
-# o comando "for" tem a função de verificar se lista[i] > lista[j];
+# o range para a letra i e o intervalo entre a primeira posicao (0), e a ultima (N-1),
+# em relacao a j, o primeiro termo foi indicado como "for i", por isso comecamos com o comando i+j
+# o comando "for" tem a funcao de verificar se lista[i] > lista[j];
 
 for i in range(0, N, 1): # possiveis valores de i
     for j in range(i+1, N, 1): # possiveis valores de j
@@ -23,3 +31,12 @@ print("lista crescente") # mostrou os valores em ordem crescente
 print("lista dos 5 maiores", lista[15:21:1]) # mostrou os 5 maiores valores
 print("lista dos 5 menores", lista[0:5:1]) # mostrou os 5 menores valores
 
+# Tarefa 2
+
+import matplotlib.pyplot as plt
+plt.figure ()
+plt.plot(range(0, N, 1), lista,'ok') 
+plt.title("Crescente")
+plt.xlabel("posicao")
+plt.ylabel("valores")
+plt.savefig("fig/bubble-fim.png")
